5944d70c5ea03f07a416d80fdf1b00f0bdaa4408
Teodor92 <theodor_kyrtev@abv.bg>
2013-10-18 17:33:28 +0300

    First java homework.

---
+++ Java/01. FuelCalculator/src/FuelCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Teodor Kurtev
+ */
+public class FuelCalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final int FuleBarier = 10;
+
+        // Data input
+        System.out.println("Enter starting Kilometres:");
+        int startingKilometres = input.nextInt();
+
+        System.out.println("Enter endingKilometres:");
+        int endingKilometres = input.nextInt();
+
+        System.out.println("Spent Fuel:");
+        float spentFuel = input.nextFloat();
+
+        // Calulations
+        int distance = endingKilometres - startingKilometres;
+        float fuelPerKilometre = spentFuel / distance;
+        float fulePerHundredKilometres = fuelPerKilometre * 100;
+
+        // Output
+        if (fulePerHundredKilometres < FuleBarier) {
+
+            System.out.printf("Your car burns %f liters of fuel per 100 kilometers. Congratulations! \n", fulePerHundredKilometres);
+
+        } else if (fulePerHundredKilometres > FuleBarier) {
+
+            System.out.printf("Your car burns %f liters of fuel per 100 kilometers. You have a problem! \n", fulePerHundredKilometres);
+
+        } else {
+            
+            System.out.printf("Your car burns %f liters of fuel per 100 kilometers. On the spot! \n", fulePerHundredKilometres);
+        }
+
+    }
+}
