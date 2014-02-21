52741d3623be07d9721026945280747845b05c27
Teodor92 <teodor.ivanov92@gmail.com>
2014-02-21 22:52:29 +0200

    Finished the second CSharp homework.

--- CSharp/02. GlobalizationAndLocalization/04. UniversityProgramAnalyser/Program.cs
+++ CSharp/02. GlobalizationAndLocalization/04. UniversityProgramAnalyser/Program.cs
@@ -1,34 +1,55 @@
 ﻿namespace _04.UniversityProgramAnalyser
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Globalization;
+    using System.Threading;
+
+    using Homework.Common;
 
     /// <summary>
     /// Counts how many times a chosen day of week is repeated between two dates
     /// </summary>
     public class Program
     {
+        private const string StartDatePrompt = "Please enter start date in dd/MM/yyyy format";
+        private const string EndDatePrompt = "Please enter end date in dd/MM/yyyy format";
+        private const string WrongFormatError = "Wrong date format";
+        private const string DayOfTheWeekPrompt = "Please enter a day of the week - Monday, Sunday etc.";
+        private const string DayOfTheWeekErrorMessage = "Invalid day of the week";
+
         internal static void Main(string[] args)
         {
-            var startDate = new DateTime(2014, 1, 1);
-            var endDate = new DateTime(2014, 2, 2);
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-            var result = (endDate - startDate).Days;
-            var currentDay = startDate;
+            var startDate = ConsoleInputHelper.SafeRead<DateTime>(StartDatePrompt, WrongFormatError);
+            var endDate = ConsoleInputHelper.SafeRead<DateTime>(EndDatePrompt, WrongFormatError);
+            var dayOfTheWeek = ConsoleInputHelper.SafeRead<DayOfWeek>(DayOfTheWeekPrompt, DayOfTheWeekErrorMessage);
+
+            if (startDate > endDate)
+            {
+                var oldValue = startDate;
+                startDate = endDate;
+                endDate = oldValue;
+            }
+
             var numberOfDays = 0;
 
             for (var i = startDate; i != endDate; i = i.AddDays(1))
             {
-                if (i.DayOfWeek == DayOfWeek.Thursday)
+                if (i.DayOfWeek == dayOfTheWeek)
                 {
                     numberOfDays++;
                 }
             }
 
-            Console.WriteLine(numberOfDays);
+            var message = string.Format(
+                "The number of {0} between {1} and {2} is {3}", 
+                dayOfTheWeek, 
+                startDate.ToString("dd/MM/yyyy"), 
+                endDate.ToString("dd/MM/yyyy"), 
+                numberOfDays);
+
+            ConsoleOutputHelper.WriteMessage(message, ConsoleMessageType.Success);
         }
     }
 }
--- CSharp/02. GlobalizationAndLocalization/05. SSNAnalyser/Program.cs
+++ CSharp/02. GlobalizationAndLocalization/05. SSNAnalyser/Program.cs
@@ -1,18 +1,122 @@
-﻿namespace _05.SSNAnalyser
+﻿using System.Collections;
+
+namespace _05.SSNAnalyser
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Globalization;
+    using System.Threading;
+
+    using Homework.Common;
 
     /// <summary>
     /// Extract someone’s gender, age, date of birth and number of days until their next birthday for Personal ID. 
     /// </summary>
     public class Program
     {
+        private const string PinInputGreeting = "Please enter your pin";
+        private const string InvalidPinPromt = "Invalid pin. Please try again";
+
         internal static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            ExtractBirthDateFormString("9204028463");
+
+            var pinString = string.Empty;
+
+            while (true)
+            {
+                pinString = ConsoleInputHelper.SafeRead<string>(PinInputGreeting);
+
+                var parsedPin = 0L;
+                var isNummeric = long.TryParse(pinString, out parsedPin);
+                var isCorrectLength = pinString.Length == 10;
+
+                if (isNummeric && isCorrectLength)
+                {
+                    break;
+                }
+
+                ConsoleOutputHelper.WriteMessage(InvalidPinPromt, ConsoleMessageType.Error);
+            }
+
+            var birthDay = ExtractBirthDateFormString(pinString);
+            var gender = ExtractGender(pinString);
+            var age = ExtratAge(pinString);
+            var daysUntilBirthDay = ExtractDaysUntilBirthDay(pinString);
+
+            var message =
+                string.Format(
+                    "The person wtih the PIN {0} is {1} and is born on: {2}. He is {3} years old and has {4} days until his birthday.",
+                    pinString, 
+                    gender, 
+                    birthDay.ToString("dd/MM/yyyy"), 
+                    age, 
+                    daysUntilBirthDay);
+
+            ConsoleOutputHelper.WriteMessage(message, ConsoleMessageType.Success);
+        }
+
+        private static DateTime ExtractBirthDateFormString(string pin)
+        {
+            var year = int.Parse(pin.Substring(0, 2));
+            var month = int.Parse(pin.Substring(2, 2));
+            var day = int.Parse(pin.Substring(4, 2));
+
+            if (month > 20)
+            {
+                year += 1800;
+            }
+            else if (month > 40)
+            {
+                year += 2000;
+            }
+            else
+            {
+                year += 1900;
+            }
+
+            var birthDay = new DateTime(year, month, day);
+            return birthDay;
+        }
+
+        private static int ExtratAge(string pin)
+        {
+            const double ApproxDaysPerYear = 365.25;
+
+            var birthDay = ExtractBirthDateFormString(pin);
+            var age = (int)((DateTime.Now - birthDay).Days / ApproxDaysPerYear);
+
+            return age;
+        }
+
+        private static int ExtractDaysUntilBirthDay(string pin)
+        {
+            var birthDay = ExtractBirthDateFormString(pin);
+            var today = DateTime.Today;
+            var next = birthDay.AddYears(today.Year - birthDay.Year);
+
+            if (next < today)
+            {
+                next = next.AddYears(1);
+            }
+
+            int numberOfDays = (next - today).Days;
+
+            return numberOfDays;
+        }
+
+        private static Gender ExtractGender(string pin)
+        {
+            var genderDigit = (int)char.GetNumericValue(pin[8]);
+
+            if (genderDigit % 2 == 0)
+            {
+                return Gender.Male;
+            }
+            else
+            {
+                return Gender.Female;
+            }
         }
     }
 }
