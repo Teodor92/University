efc8eebbe87423e3e68e14a23cb0f2dee6f60d88
Teodor92 <teodor.ivanov92@gmail.com>
2014-03-23 16:00:34 +0200

    Added project structure and base layout to the project.

---
+++ Internet Technologies Project/SimpleToDoApp/SimpleToDoApp/scripts/siteController.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    var siteContentContaierId = "#body-content";
+
+    // start page load
+    viewsFactory.getTemplate("home").done(function (data) {
+        $(siteContentContaierId).html(data);
+    });
+
+    // nav pages click load
+    $("#navigation-link").on("click", "a", function (e) {
+        var view = $(this).data("view");
+
+        viewsFactory.getTemplate(view).done(function (data) {
+            $(siteContentContaierId).html(data);
+        });
+    });
+});
---
+++ Internet Technologies Project/SimpleToDoApp/SimpleToDoApp/scripts/views/viewFactory.js
@@ -0,0 +1,33 @@
+﻿window.viewsFactory = (function () {
+    var rootUrl = "partials/";
+
+    var templates = {};
+
+    function getTemplate(name) {
+
+        var deferred = Q.defer();
+
+        if (templates[name]) {
+            deferred.resolve(templates[name]);
+        }
+        else {
+            $.ajax({
+                url: rootUrl + name + ".html",
+                type: "GET",
+                success: function (templateHtml) {
+                    templates[name] = templateHtml;
+                    deferred.resolve(templateHtml);
+                },
+                error: function (err) {
+                    deferred.reject(err);
+                }
+            });
+        }
+
+        return deferred.promise;
+    }
+
+    return {
+        getTemplate: getTemplate
+    };
+}());
